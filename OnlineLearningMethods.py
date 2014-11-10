d28fe3eae830b471279ad9cc74e0f51b3e5a0072
turboNinja2 <julien.revelle@gmail.com>
2014-11-10 21:42:50 +0100

    commit

--- OnlineLearningMethods.py
+++ OnlineLearningMethods.py
@@ -17,21 +17,22 @@ class Model:
   def __str__(self):
    return "%s, params : %s" % (self.name, str(self.params))
  
-  # C.  Get probability estimation on x
-  # INPUT:
-  #   x: features
-  #   w: weights
-  # OUTPUT:
-  #   probability of p(y = 1 | x; w)
-  def predict(self, x):
+
+   def innerProduct(self,x):
     wTx = 0.
+    n = 0
     for i in x:  # do wTx
-      wTx += (self.w[i]) * 1.  # w[i] * x[i], but if i in x we got x[i] = 1.
+      wTx += self.w[i] * 1.  # w[i] * x[i], but if i in x we got x[i] = 1.
+      n+=1
+    return wTx,n
+
+  def predict(self, x):
+    wTx = innerProduct(self,x)
     return 1. / (1. + exp(-max(min(wTx, 20.), -20.)))  # bounded sigmoid
+
 
   def getLogLoss(self):
     return self.loss * 1. / self.nbIterations
-
 
   def train(self, trainPath,customRefreshLine=None):
     if customRefreshLine is not None:
@@ -51,7 +52,6 @@ class OnlineLinearLearning(Model):
     Model.__init__(self,params, wInit)
     self.n = [0] * len(wInit)
     self.name = "Online method"
-
 
   ## D.  Update given model
   # INPUT:
@@ -106,22 +106,18 @@ class PA(Model):
     Model.__init__(self,params, wInit)
     self.name = "PA"
 
-  def innerProduct(self,x):
-    wTx = 0.
-    n = 0
-    for i in x:  # do wTx
-      wTx += self.w[i] * 1.  # w[i] * x[i], but if i in x we got x[i] = 1.
-      n+=1
-    return wTx,n
-
   def update(self, x, y):
     wTx,n = self.innerProduct(x)
     p = copysign(1,wTx)
     sufferLoss = max(0,1 - y * wTx)
     self.nbIterations += 1
     self.loss += logloss(p, y)  # for progressive validation
-    tau = sufferLoss / (n**2)
+    tau = sufferLoss / n
     for i in x:
       self.w[i] -= tau * y * 1. 
 
   def predict(self,x):
+    wTx,n = self.innerProduct(x)
+    p = (1 + copysign(1,wTx)) / 2.
+    return p
+

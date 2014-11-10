9b40c45635a0914d7855f1d50e609e3bc3a2f353
hulyce <ulysseklatzmann@gmail.com>
2014-11-10 23:32:17 +0100

    lm

--- test.py
+++ test.py
@@ -5,9 +5,10 @@ from settings               import *
 from Learn                  import *
 from ErrorEvaluation        import *
 from Model                  import *
+from Models                 import *
 from Globals                import *
 
-refreshLine = 100
+refreshLine = 150
 MULTI = True
 
 params = {"alpha" : 0.1}   # learning rate for sgd optimization
@@ -35,6 +36,7 @@ else:
 
 
 if MULTI:
-  models = [Learning(params, w)] * 2
-  trainModels(train, models)
-  print validationErrors(validation, models)
+  model_list = [Learning(params, w)] * 2
+  models = Models(model_list)
+  models.train(train)
+  models.validation(validation)

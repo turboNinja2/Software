87174bc01ddb85fbb56222a02da5bf8ae482ce0c
hulyce <ulysseklatzmann@gmail.com>
2014-11-10 20:23:30 +0100

    Model class added

--- OnlineLearningMethods.py
+++ OnlineLearningMethods.py
@@ -4,20 +4,17 @@ from ErrorEvaluation import logloss
 class Model:
 
   def __init__(self, params, wInit):
-    self.params = params
-    self.w = wInit
-
-class OnlineLinearLearning(Model):
-  def __init__(self,params,wInit):
-    self.alpha = params[0]
-    self.w = wInit
-    self.n = [0] * len(wInit)
+    self.params       = params
+    self.w            = wInit
     self.nbIterations = 0
-    self.loss = 0.
+    self.loss         = 0
+    self.name         = "Unamed"
+    for key in params.keys():
+      setattr(self, key, params[key])
 
-  def description(self):
-    return 'On line method, alpha =' + str(self.alpha)
-
+  def __str__(self):
+   return "%s, params : %s" % (self.name, str(params))
+ 
   # C.  Get probability estimation on x
   # INPUT:
   #   x: features
@@ -29,6 +26,18 @@ class OnlineLinearLearning(Model):
     for i in x:  # do wTx
       wTx += (self.w[i]) * 1.  # w[i] * x[i], but if i in x we got x[i] = 1.
     return 1. / (1. + exp(-max(min(wTx, 20.), -20.)))  # bounded sigmoid
+
+
+  def getLogLoss(self):
+    return self.loss * 1. /  self.nbIterations
+    
+
+class OnlineLinearLearning(Model):
+  def __init__(self,params,wInit):
+    Model.__init__(self,params, wInit)
+    self.n      = [0] * len(wInit)
+    self.name   = "Online method"
+
 
   ## D.  Update given model
   # INPUT:
@@ -53,38 +62,14 @@ class OnlineLinearLearning(Model):
       self.n[i] += abs(p - y)
       self.w[i] -= (p - y) * 1. * self.alpha / sqrt(self.n[i])
 
-  def getLogLoss(self):
-    return self.loss * 1. /  self.nbIterations
-    
 class ZALMS(Model):
   def __init__(self,params,wInit):
-    self.delta = params[0]
-    self.rho = params[1]
-    self.w = wInit
-    self.nbIterations = 0
-    self.loss = 0.
-  
-  def description(self):
-    return 'ZALMS, delta =' + str(self.delta) + ' rho = ' + str(self.rho)
+    Model.__init__(self,params, wInit)
+    self.name = "ZALMS"
 
-  # C.  Get probability estimation on x
-  # INPUT:
-  #   x: features
-  #   w: weights
-  # OUTPUT:
-  #   probability of p(y = 1 | x; w)
-  def predict(self, x):
-    wTx = 0.
-    for i in x:  # do wTx
-      wTx += (self.w[i]) * 1.  # w[i] * x[i], but if i in x we got x[i] = 1.
-    return 1. / (1. + exp(-max(min(wTx, 20.), -20.)))  # bounded sigmoid
-  
   def update(self, x, y):
     p = self.predict(x)
     self.nbIterations += 1
     self.loss += logloss(p, y)  # for progressive validation
     for i in x:
       self.w[i] -= self.delta * ((p - y) * 1. + self.rho * copysign(self.w[i],1))
-
-  def getLogLoss(self):
-    return self.loss * 1. /  self.nbIterations

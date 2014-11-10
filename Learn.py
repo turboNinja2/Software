a8bc7394662a41214151407e91884470df88254c
turboNinja2 <julien.revelle@gmail.com>
2014-11-10 17:46:09 +0100

    Code optimization

--- Learn.py
+++ Learn.py
@@ -8,35 +8,13 @@ from multiprocessing import Pool
 from joblib import Parallel, delayed  
 import multiprocessing
 
-
 def trainModel(trainPath,model):
-    n = [0.] * D
-    loss = 0.
     tt = 1
     data = DataParser(trainPath) 
-
     for ID, x, y in data.run():
-        p = model.predict(x)
-        model.update(n, x, y)
-        loss += logloss(p, y)  # for progressive validation
+        model.update(x, y)
         # print out progress, so that we know everything is working
         if tt % refreshLine == 0:
-         print('%s\tencountered: %d\tcurrent logloss: %f' % (datetime.now(), tt, (loss * 1. / tt)))
+         print('%s\tencountered: %d\tcurrent logloss: %f' % (datetime.now(), tt, model.getLogLoss()))
         tt += 1
-
         
-
-def trainModels(trainPath,models):
-    n = [0.] * D
-    loss = 0.
-    tt = 1
-    data = DataParser(trainPath) 
-    nbModels = len(models)
-
-    for ID, x, y in data.run():
-
-      def innerFunction(model):
-        model.update(n, x, y)
-
-      for i in range(0,nbModels):
-        models[i].update(n, x, y)

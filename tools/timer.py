0c721fa1c8e58cc343a3800a25030d62daa759ab
hulyce <ulysseklatzmann@gmail.com>
2014-11-10 19:02:22 +0100

    jkl

--- tools/timer.py
+++ tools/timer.py
@@ -9,49 +9,55 @@ import matplotlib.pyplot as plt
 
 class Timer():
 	
-	def __init__(self):
-		self._time_list = []
-		self._last_time = None
-		self._time_dict = {}
+  def __init__(self):
+    self._time_list = []
+    self._last_time = None
+    self._time_dict = {}
 
 
-	def clean(self):
-		self._time_list = []
+  def clean(self):
+    self._time_list = []
 
-	def pick(self, flag=None):
-		if self._last_time is not None:
-			current_time = time()-self._last_time
-			self._time_list.append((current_time,flag))
-		self._last_time = time()
+  def pick(self, flag=None):
+    if self._last_time is not None:
+      current_time = time()-self._last_time
+      self._time_list.append((current_time,flag))
+      if flag is not None:
+        try:
+          self._time_dict[flag].append(current_time)
+        except:
+          self._time_dict[flag] = [current_time]
+    self._last_time = time()
 
 
-	def save(self):
-		for t, flag in self._time_list:
-			if flag is not None:
-				if flag not in self._time_dict.keys():
-					self._time_dict[flag] = []
-				self._time_dict[flag].append(t)
+  def save(self):
+    for t, flag in self._time_list:
+      if flag is not None:
+        if flag not in self._time_dict.keys():
+          self._time_dict[flag] = []
+        self._time_dict[flag].append(t)
 
-	def prnt(self,average=0):
-		print "printing Timer : "
-		if not average:
-			for t, flag in self._time_list:
-				if flag is not None:
-					print "%s : %s" % (flag, t)
-				else:
-					print "%s" % t
-		else:
-			for flag in self._time_dict.keys():
-				_sum = sum(self._time_dict[flag])
-				avg = _sum/len(self._time_dict[flag])
-				print "avg %s : %s" % (flag, avg)
-				print "sum %s : %s" % (flag, _sum)
+  def prnt(self,average=0,p_sum=False):
+    print "printing Timer : "
+    if not average:
+      for t, flag in self._time_list:
+        if flag is not None:
+          print "%s : %s" % (flag, t)
+        else:
+          print "%s" % t
+    else:
+      for flag in self._time_dict.keys():
+        _sum = sum(self._time_dict[flag])
+        avg = _sum/len(self._time_dict[flag])
+        print "avg %s : %s" % (flag, avg)
+        if p_sum:
+          print "sum %s : %s" % (flag, _sum)
 
-	def draw(self):
-		color = "r--"
-		time_list = []
-		for t, flag in self._time_list:
-			time_list.append(t)
-		args = (range(len(time_list)), time_list, color)
-		plt.plot(*args)
-		plt.show()
+  def draw(self):
+    color = "r--"
+    time_list = []
+    for t, flag in self._time_list:
+      time_list.append(t)
+    args = (range(len(time_list)), time_list, color)
+    plt.plot(*args)
+    plt.show()

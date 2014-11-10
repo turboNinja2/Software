f51ffc18a30c44e34d49e8f5aba185d576513dc2
hulyce <ulysseklatzmann@gmail.com>
2014-11-10 12:13:56 +0100

    test

--- Main.py
+++ Main.py
@@ -10,11 +10,10 @@ from ErrorEvaluation        import *
 from settings               import *
 
 # folders #################################################################
-dataPath = 'C:/Users/JUJulien/Desktop/KAGGLE/Competitions/Avazu/Data/'
-train_global = dataPath + 'train_rev2.csv'  # path to training file
-test_global = dataPath + 'test_rev2.csv'  # path to testing file
-train_set = dataPath + 'train_set.csv'  # path to training file
-validation_set = dataPath + 'validation_set.csv'  # path to testing file
+train_global    = dataPath + 'train_rev2.csv'  # path to training file
+test_global     = dataPath + 'test_rev2.csv'  # path to testing file
+train_set       = dataPath + 'train_set.csv'  # path to training file
+validation_set  = dataPath + 'validation_set.csv'  # path to testing file
 
 # training and testing #######################################################
 alpha = .1   # learning rate for sgd optimization

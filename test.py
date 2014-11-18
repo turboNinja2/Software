68cf26c74478ff76dc901a5ca06823ac2f9f8385
hulyce <ulysseklatzmann@gmail.com>
2014-11-18 21:08:42 +0100

    test refactored

--- test.py
+++ test.py
@@ -7,51 +7,58 @@ from Globals                import *
 
 MULTI = True
 
-
-Learning    = OnlineLinearLearning
-
-train       = dataPath + "small_train_set.csv"
-validation  = dataPath + "small_validation_set.csv"
-dump        = dataPath + "results/test_results.csv"
-json_dump   = dataPath + "results/test_json_results.csv" 
-
-kwargs = {
-  "trainPath"       : train,
-  "validationPath"  : validation,
-  "dumpingPath"     : dump,
-  "jsonDumpingPath" : json_dump,
-  "refreshLine"     : 100,
-}
+class Test:
 
 
-expected    = 0.167259060835432709423848
+  def __init__(self):
+    self.Learning    = OnlineLinearLearning
+
+    train       = dataPath + "small_train_set.csv"
+    validation  = dataPath + "small_validation_set.csv"
+    dump        = dataPath + "results/test_results.csv"
+    json_dump   = dataPath + "results/test_json_results.csv" 
+
+    self.kwargs = {
+      "trainPath"       : train,
+      "validationPath"  : validation,
+      "dumpingPath"     : dump,
+      "jsonDumpingPath" : json_dump,
+      "refreshLine"     : 100,
+    }
 
 
-model = Learning({"alpha" : 0.1}, 
-                 [0.] * D, 
-                 **kwargs
-                 )
-model.train()
-found = model.validate()
-model.dump_score()
+    self.expected    = 0.167259060835432709423848
 
-if found == expected:
-  print("We're Good !")
-else:
-  print("Houston, we got a problem. Found : %s, Expected : %s" % (found, expected))
+  def run(self):
+    model = self.Learning({"alpha" : 0.1}, 
+                     [0.] * D, 
+                     **self.kwargs
+                     )
+    model.train()
+    found = model.validate()
+    model.dump_score()
 
+    if found == self.expected:
+      print("We're Good !")
+    else:
+      print("Houston, we got a problem. Found : %s, Expected : %s" % (found, self.expected))
+
+  def run_multi(self):
+    model_list = []
+    for i in range(num_cores):
+      model_list.append(self.Learning({"alpha" : 10 ** (-i)}, 
+                    [0] * D,
+                    **self.kwargs
+                    ))
+
+    models = Models(model_list)
+    models.train()
+    models.validation()
+    models.dump()
+
+    print("Test ended")
+
+t = Test()
+t.run()
 if MULTI:
-  model_list = []
-  for i in range(num_cores):
-    model_list.append(OnlineLinearLearning({"alpha" : 10 ** (-i)}, 
-                  [0] * D,
-                  **kwargs
-                  ))
-
-  models = Models(model_list)
-  models.train()
-  models.validation()
-  models.dump()
-
-print("Test ended")
-
+  t.run_multi()
